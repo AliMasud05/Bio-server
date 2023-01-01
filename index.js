38b20c61f6386518dff5397e0d037c0dfc419185
AliMasud05 <morshedalimasud3@gmail.com>
2023-01-01 20:47:13 +0600

    basic

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const port = process.env.PORT ||5000;
+const port = process.env.PORT || 5000;
 require('dotenv').config();
+
 
 //middle wares
 app.use(cors());
@@ -11,48 +13,41 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://greendays:oKwznZxfspKGlyq9@cluster0.memgfjc.mongodb.net/?retryWrites=true&w=majority`;
+const uri = "mongodb+srv://greendays:oKwznZxfspKGlyq9@cluster0.memgfjc.mongodb.net/?retryWrites=true&w=majority";
 //console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
- 
-async function run(){
-    try{
-        const serviceCollection = client.db('greenDays').collection('homeService');
-        
-        app.get('/services', async(req, res)=>{
+
+async function run() {
+    try {
+        const homeServiceCollection = client.db('greendays').collection('homeService');
+        app.get('/homeservice', async (req, res) => {
             const query = {};
-            const cursor = serviceCollection.find(query);
+            const cursor =homeServiceCollection.find(query);
             const services = await cursor.toArray();
-            res.send(services);
+            console.log(services);
         });
-        app.get('/service', async(req, res)=>{
-            const query = {};
-            const cursor = serviceCollection.find(query);
-            const services = await (await cursor.toArray()).slice(0,3);
-            res.send(services);
-        });
-        app.get('/services/:id', async (req, res) => {
-            const id = req.params.id;
-            console.log(id);
-            const query = { _id:ObjectId(id) };
-            const service = await serviceCollection.findOne(query);
-            res.send(service);
-        });
-        
-        
+
+       
+
+
+
+
+
     }
-    finally{
+    finally {
 
     }
 
 }
-run().catch(err=>console.error(err));
+run().catch(err => console.error(err));
+
+
 
 app.get('/', (req, res) => {
-    res.send('server is running');
+    res.send('server ids running');
 });
 
 
 app.listen(port, () => {
-    console.log(`ecohouse server running on ${port}`);
+    console.log(` server running on ${port}`);
 })
